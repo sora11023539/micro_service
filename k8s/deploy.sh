d89d268fc315efcc582a863faf65ea6c3412083d
sora11023539 <sora11023539@gmail.com>
2024-03-04 01:42:44 +0900

    Add Dockerfile.deploy for frontend deployment and update deploy script

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -11,6 +11,10 @@ docker login -u ${USER_NAME} -p ${PASSWORD} ${DOCKER_REGISTRY_DOMAIN}
 
 for service in ${SERVICE_NAMES[@]}; do
   cd ../${service}
-  docker build --platform linux/amd64 -t ${DOCKER_REGISTRY}:${service} .
+  if [ ${service} = "frontend" ]; then
+    docker build --platform linux/amd64 -t ${DOCKER_REGISTRY}:${service} -f Dockerfile.deploy .
+  else
+    docker build --platform linux/amd64 -t ${DOCKER_REGISTRY}:${service} .
+  fi
   docker push ${DOCKER_REGISTRY}:${service}
 done
